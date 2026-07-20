11713902ac79dda10d66559ac192467f8b5f420f
Sleep1223 <sleep1223@outlook.com>
2026-07-20 17:29:44 +0800

    fix(build): update SDK internal build number to 2026072001 refactor(pintelemetry): optimize damage event handling and improve data structure

--- src/core/build_version.h
+++ src/core/build_version.h
@@ -2,4 +2,4 @@
 
 // Increment this number for every code change that should be identifiable in
 // server startup logs.
-static constexpr unsigned int SDK_INTERNAL_BUILD_NUMBER = 2026071801u;
+static constexpr unsigned int SDK_INTERNAL_BUILD_NUMBER = 2026072001u;
